b0eb993d882cacbf2e1e60e3a1cdd9d3bac6299b
SATWIKOJHA <ojhasatwik@gmail.com>
2025-07-16 14:53:39 +0530

    Intial Commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "f6133de80a70a322bb295cd9c9f20348";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
